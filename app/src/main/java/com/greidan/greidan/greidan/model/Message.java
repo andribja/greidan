5db4d00d19e519752e000c0ec1112e5dbcb6bed4
FANNAR KRISTINSSON <FANNAR KRISTINSSON>
2016-04-15 16:31:48 +0000

    Added a Message class

---
+++ app/src/main/java/com/greidan/greidan/greidan/model/Message.java
@@ -0,0 +1,178 @@
+package com.greidan.greidan.greidan.model;
+
+import android.content.ContentValues;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.greidan.greidan.greidan.DbSchema;
+import com.greidan.greidan.greidan.manager.UserManager;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Message implements Parcelable {
+
+    long id;
+    String subject;
+    String content;
+    long recipientId;
+    long authorId;
+    String authorName;
+    Date timePosted;
+
+    public Message() {
+
+    }
+
+    public Message(long id, String subject, String content, long recipientId, long authorId, String authorName, Date timePosted) {
+        this.id = id;
+        this.subject = subject;
+        this.content = content;
+        this.recipientId = recipientId;
+        this.authorId = authorId;
+        this.authorName = authorName;
+        this.timePosted = timePosted;
+    }
+
+    public Message(JSONObject jsonMessage) throws JSONException, ParseException {
+        UserManager userManager = new UserManager(null);
+
+        this.id = jsonMessage.getLong("_id");
+        this.subject = jsonMessage.getString("subject");
+        this.content = jsonMessage.getString("content");
+        this.recipientId = jsonMessage.getLong("recipient_id");
+        this.authorId = jsonMessage.getLong("author_id");
+        this.authorName = jsonMessage.getString("author_name");
+        this.timePosted = new Date(jsonMessage.getLong("timePosted"));
+    }
+
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+
+        values.put(DbSchema.MessageTable.Cols.ID, Long.toString(id));
+        values.put(DbSchema.MessageTable.Cols.SUBJECT, subject);
+        values.put(DbSchema.MessageTable.Cols.CONTENT, content);
+        values.put(DbSchema.MessageTable.Cols.AUTHOR_ID, authorId);
+        values.put(DbSchema.MessageTable.Cols.RECIPIENT_ID, recipientId);
+        values.put(DbSchema.MessageTable.Cols.AUTHOR_NAME, authorName);
+        values.put(DbSchema.MessageTable.Cols.TIME_POSTED, timePosted.toString());
+
+        return values;
+    }
+
+    public ArrayList<NameValuePair> getAsRequestParams() {
+        ArrayList<NameValuePair> requestParams = new ArrayList<NameValuePair>();
+        requestParams.add(new BasicNameValuePair("subject", subject));
+        requestParams.add(new BasicNameValuePair("content", content));
+        requestParams.add(new BasicNameValuePair("author_id", Long.toString(authorId)));
+        requestParams.add(new BasicNameValuePair("recipient_id", Long.toString(recipientId)));
+
+        return requestParams;
+    }
+
+
+    /* Implement Parcelable */
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(subject);
+        dest.writeString(content);
+        dest.writeLong(authorId);
+        dest.writeLong(recipientId);
+        dest.writeString(timePosted.toString());
+    }
+
+    public Message(Parcel in) throws ParseException {
+        UserManager userManager = new UserManager(null);
+
+        id = in.readLong();
+        subject = in.readString();
+        content = in.readString();
+        authorName = in.readString();
+        authorId = in.readLong();
+        recipientId = in.readLong();
+        timePosted = new Date(in.readLong());
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Message createFromParcel(Parcel in) {
+            try {
+                return new Message(in);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
+
+    /* Getters and setters */
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public long getRecipientId() {
+        return recipientId;
+    }
+
+    public Date getTimePosted() {
+        return timePosted;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String subject) {
+        this.subject = subject;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setRecipient(long recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public void setTimePosted(Date timePosted) {
+        this.timePosted = timePosted;
+    }
+}

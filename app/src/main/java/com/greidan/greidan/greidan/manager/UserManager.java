760594ef08d989c4811ff17d217e8e95bec57fe6
dingari <danielgretars@bitbucket.org>
2016-04-16 21:37:03 +0000

    implemented user profile fetching in user manager

--- app/src/main/java/com/greidan/greidan/greidan/manager/UserManager.java
+++ app/src/main/java/com/greidan/greidan/greidan/manager/UserManager.java
@@ -1,6 +1,7 @@
 package com.greidan.greidan.greidan.manager;
 
 import android.app.Activity;
+import android.app.job.JobInfo;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,17 +14,24 @@ import com.greidan.greidan.greidan.activity.ProgressActivity;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.jar.Attributes;
 
 public class UserManager {
 
     SharedPreferences prefs;
     Activity activity;
     String loginUrl;
+    String logoutUrl;
     String registerUrl;
+    String profileUrl;
 
     public UserManager(Activity activity) {
         this.activity = activity;
@@ -33,18 +41,27 @@ public class UserManager {
             String host = activity.getString(R.string.host);
             String port = activity.getString(R.string.port);
             loginUrl = host + ":" + port + "/login";
+            logoutUrl = host + ":" + port + "/logout";
             registerUrl = host + ":" + port + "/register";
+            profileUrl = host + ":" + port + "/user";
         }
     }
 
     public void register(String username, String email, String password) {
-        AuthTask registerTask = new AuthTask((ProgressActivity) activity, username, email, password, registerUrl);
+        ArrayList<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("password", password));
+        UserTask registerTask = new UserTask((ProgressActivity) activity, registerUrl, params, true);
 
         registerTask.execute();
     }
 
     public void login(String username, String password) {
-        AuthTask loginTask = new AuthTask((ProgressActivity) activity, username, null, password, loginUrl);
+        ArrayList<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        UserTask loginTask = new UserTask((ProgressActivity) activity, loginUrl, params, true);
 
         loginTask.execute();
     }
@@ -80,81 +97,132 @@ public class UserManager {
         return prefs.getString("token", "");
     }
 
-    public User getLoggedInUser() {
-        // TODO: properly implement
-        return new User(1337, "temp_user", "temp@user.com");
-    }
-
     public String getLoggedInUsername() {
         return prefs.getString("username", "");
     }
 
-    public User findUserByUsername(String username) {
-        // TODO: Implement this; query database or contact server to find user
-        return new User(0, username, null);
+//    public User findUserByUsername(String username) {
+//        // TODO: Implement this; query database or contact server to find user
+//        return new User(0, username, null);
+//    }
+
+//    public User getUserById(int id) {
+//        // TODO: Implement this: query database or contact server to find user
+////        return new User(id, "user_from_id", "userfrom@id.com");
+//    }
+    
+    public void fetchLoggedInUserProfile() {
+        ArrayList<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("token", getToken()));
+
+        UserTask task = new UserTask((ProgressActivity) activity, profileUrl, params, false);
+        task.execute();
     }
 
-    public User getUserById(int id) {
-        // TODO: Implement this: query database or contact server to find user
-        return new User(id, "user_from_id", "userfrom@id.com");
+    private void handleRequestedData(JSONObject jObj, Bundle response) {
+
+        if(jObj != null) {
+            Iterator<?> keys = jObj.keys();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+
+                if(key.equals("userlist")) {
+                    ArrayList<User> users = new ArrayList<>();
+                    try {
+                        JSONArray jArr = jObj.getJSONArray(key);
+                        for(int i=0; i<jArr.length(); i++) {
+                            users.add(new User(jArr.getJSONObject(i)));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    response.putParcelableArrayList("users", users);
+                }
+
+                if(key.equals("user")) {
+                    try {
+                        response.putParcelable("user", new User(jObj.getJSONObject(key)));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        ((ProgressActivity) activity).doUponCompletion(response);
     }
 
     // An async task that tries to authenticate the user against the server
-    private class AuthTask extends AsyncTask<Void, Void, JSONObject> {
+    private class UserTask extends AsyncTask<Void, Void, JSONObject> {
 
-        String username;
-        String email;
-        String password;
         String url;
+        List<NameValuePair> requestParams;
+        boolean post;
+
         ProgressActivity activity;
 
-        public AuthTask(ProgressActivity activity, String username, String email, String password, String url) {
-            this.username = username;
-            this.email = email;
-            this.password = password;
+        public UserTask(ProgressActivity activity, String url, List<NameValuePair> params, boolean post) {
             this.url = url;
+            this.requestParams = params;
+            this.post = post;
 
             this.activity = activity;
         }
 
         @Override
         protected JSONObject doInBackground(Void... params) {
-            ArrayList<NameValuePair> requestParams = new ArrayList<NameValuePair>();
-            requestParams.add(new BasicNameValuePair("username", username));
-            requestParams.add(new BasicNameValuePair("email", email));
-            requestParams.add(new BasicNameValuePair("password", password));
-
             ServerRequest request = new ServerRequest();
-            return request.postToUrl(url, requestParams);
+
+            if(post) {
+                return request.postToUrl(url, requestParams);
+            } else {
+                return request.getFromUrl(url, requestParams);
+            }
+
         }
 
         @Override
         protected void onPostExecute(JSONObject jObj) {
+            Bundle response = new Bundle();
             Boolean success = false;
             String message = "";
 
-            try { success = jObj.getBoolean("success"); }
-            catch (JSONException | NullPointerException e) { e.printStackTrace(); }
+            if(jObj == null) {
+                response.putBoolean("error", true);
+            } else {
+                try { success = jObj.getBoolean("success"); }
+                catch (JSONException | NullPointerException e) { e.printStackTrace(); }
 
-            try { message = jObj.getString("response"); }
-            catch (JSONException e) { e.printStackTrace(); }
+                try { message = jObj.getString("response"); }
+                catch (JSONException e) { e.printStackTrace(); }
 
-            if(success) {
-                try {
-                    String token = jObj.getString("token");
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putString("token", token);
-                    editor.putString("username", username);
-                    editor.apply();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                response.putBoolean("success", success);
+                response.putString("message", message);
             }
 
-            Bundle data = new Bundle();
-            data.putBoolean("success", success);
-            data.putString("message", message);
-            activity.doUponCompletion(data);
+            if(post) {
+                if(url.equals(profileUrl)) {
+                    // For when posting profile updates
+                } else {
+                    if (success) {
+                        try {
+                            String token = jObj.getString("token");
+                            String username = requestParams.get(0).getValue();  // Temp fix to get username
+
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putString("token", token);
+                            editor.putString("username", username);
+                            editor.apply();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    activity.doUponCompletion(response);
+                }
+            } else {
+                handleRequestedData(jObj, response);
+            }
         }
 
         @Override

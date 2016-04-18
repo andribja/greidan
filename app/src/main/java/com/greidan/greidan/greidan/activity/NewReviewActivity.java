94f4fd256fdd65d5ee9253f9689d7c16e722423d
dingari <danielgretars@bitbucket.org>
2016-04-18 09:31:55 +0000

    added an activity for creating a new review

---
+++ app/src/main/java/com/greidan/greidan/greidan/activity/NewReviewActivity.java
@@ -0,0 +1,78 @@
+package com.greidan.greidan.greidan.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.greidan.greidan.greidan.R;
+import com.greidan.greidan.greidan.manager.ReviewManager;
+import com.greidan.greidan.greidan.manager.UserManager;
+import com.greidan.greidan.greidan.model.Review;
+
+import java.util.Date;
+
+public class NewReviewActivity extends ProgressActivity {
+
+    private static final String TAG = "NewReviewActivity";
+
+    ReviewManager reviewManager;
+    UserManager userManager;
+
+    TextView mRevieweeLabel;
+    EditText mContent;
+    RatingBar mRatingBar;
+    Button mButtonPost;
+
+    String revieweeName;
+    Review newReview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_review);
+
+        reviewManager = new ReviewManager(this);
+        userManager = new UserManager(this);
+
+        revieweeName = getIntent().getStringExtra("revieweeName");
+
+        mRevieweeLabel = (TextView) findViewById(R.id.new_reviewee_label);
+        mContent = (EditText) findViewById(R.id.new_review_content);
+        mRatingBar = (RatingBar) findViewById(R.id.new_review_rating);
+        mButtonPost = (Button) findViewById(R.id.button_post_review);
+        mButtonPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = mContent.getText().toString();
+                double rating = mRatingBar.getRating();
+
+                attemptPostReview(revieweeName, content, rating);
+            }
+        });
+    }
+
+    private void attemptPostReview(String revieweeName, String content, double rating) {
+        newReview = new Review(null, rating, content, revieweeName, userManager.getLoggedInUsername(), new Date());
+
+        reviewManager.postReviewToServer(newReview);
+    }
+
+    @Override
+    public void doUponCompletion(Bundle response) {
+        Log.i(TAG, "doUponCompletion");
+        Log.i(TAG, "Response: " + response);
+
+        String message = response.getString("message");
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        if(response.getBoolean("success")) {
+            finish();
+        }
+    }
+}

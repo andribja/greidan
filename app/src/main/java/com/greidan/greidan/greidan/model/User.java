4315e021440e6ef776bc108304d5f3fb455a1dac
dingari <danielgretars@bitbucket.org>
2016-04-18 01:00:10 +0000

    add local image path to user model

--- app/src/main/java/com/greidan/greidan/greidan/model/User.java
+++ app/src/main/java/com/greidan/greidan/greidan/model/User.java
@@ -4,12 +4,14 @@ import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.apache.http.NameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.Serializable;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
 
 public class User implements Parcelable {
 
@@ -18,15 +20,14 @@ public class User implements Parcelable {
     String email;
     Date timeJoined;
     String extImagePath;
-    String localImage;
+    String localImagePath;
     double rating;
 
-    public User(String id, String username, String email, Date timeJoined, String extImagePath, double rating) {
+    public User(String id, String username, String email, Date timeJoined, double rating) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.timeJoined = timeJoined;
-        this.extImagePath = extImagePath;
         this.rating = rating;
     }
 
@@ -79,6 +80,22 @@ public class User implements Parcelable {
         this.rating = rating;
     }
 
+    public String getExtImagePath() {
+        return extImagePath;
+    }
+
+    public void setExtImagePath(String extImagePath) {
+        this.extImagePath = extImagePath;
+    }
+
+    public String getLocalImagePath() {
+        return localImagePath;
+    }
+
+    public void setLocalImagePath(String localImage) {
+        this.localImagePath = localImage;
+    }
+
     /// Impelemnt parcelable
 
     @Override
@@ -93,7 +110,7 @@ public class User implements Parcelable {
         dest.writeString(email);
         dest.writeString(timeJoined.toString());
         dest.writeString(extImagePath);
-        dest.writeString(localImage);
+        dest.writeString(localImagePath);
         dest.writeDouble(rating);
     }
 
@@ -103,7 +120,7 @@ public class User implements Parcelable {
         this.email = in.readString();
         this.timeJoined = new Date(in.readString());
         this.extImagePath = in.readString();
-        this.localImage = in.readString();
+        this.localImagePath = in.readString();
         this.rating = in.readDouble();
     }
 
